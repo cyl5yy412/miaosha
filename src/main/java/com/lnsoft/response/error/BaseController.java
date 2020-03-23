dbb06b23209becb730a7c90b5cf74288e2e92e13
chryl <cyl5yy412@gmail.com>
2020-03-23 15:39:52 +0800

    init

--- src/main/java/com/lnsoft/response/error/BaseController.java
+++ src/main/java/com/lnsoft/response/error/BaseController.java
@@ -2,6 +2,7 @@ package com.lnsoft.response.error;
 
 import com.lnsoft.response.ReturnResult;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,13 +14,15 @@ import java.util.Map;
 /**
  * Created by chr on 2018/12/30/0030.
  */
+@ControllerAdvice//https://www.cnblogs.com/lenve/p/10748453.html
+//@RestControllerAdvice//异常处理增强controller,一般用于全局异常处理,配合@ExceptionHandler一起使用
 public class BaseController {
 
     public static final String CONTENT_TYPE_FORMED = "application/x-www-form-urlencoded";
 
     //定义ExceptionHandler解决未被Controller层吸收的Exception
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.OK)//异常返回也处理为200,只要是接口响应就是200,除非接口无法返回(或者使用500代表返回的信息为异常信息)
+    @ExceptionHandler(Exception.class)//定义拦截的类型
+    @ResponseStatus(HttpStatus.OK)//自定义返回状态码,异常返回也处理为200,只要是接口响应就是200,除非接口无法返回(或者使用500代表返回的信息为异常信息)
     @ResponseBody
     public Object handlerException(HttpServletRequest request, Exception ex) {//该ex就是未被吸收的exception
         Map<String, Object> responseData = new HashMap<>();
